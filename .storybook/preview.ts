8bcd6dcbea709ea7e3c07482906bb5c1ea9b47c7
anewstead <4138440+anewstead@users.noreply.github.com>
2025-04-08 20:44:47 +0100

    update deps

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,22 @@
-import type { Preview } from '@storybook/react'
+import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  // a11y setting may be required
+  // (it was added during an auto upgrade adding the addon-test)
+  // a11y: {
+  //   // 'todo' - show a11y violations in the test UI only
+  //   // 'error' - fail CI on a11y violations
+  //   // 'off' - skip a11y checks entirely
+  //   test: "todo",
+  // },
 };
 
 export default preview;
--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -1,12 +1,14 @@
 import { beforeAll } from "vitest";
 import { setProjectAnnotations } from "@storybook/react";
 import * as projectAnnotations from "./preview";
+// import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
 
 // This is an important step to apply the right configuration when testing your stories.
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
-const project = setProjectAnnotations([projectAnnotations]);
+const project = setProjectAnnotations([
+  // a11yAddonAnnotations,
 
-beforeAll(async () => {
-  // Initialize the project - the Storybook test plugin will handle browser setup
-  await project.beforeAll();
-});
+  projectAnnotations,
+]);
+
+beforeAll(project.beforeAll);
