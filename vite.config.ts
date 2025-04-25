1d65bab5c266200edcf22ec87d9caf3406fc3819
anewstead <4138440+anewstead@users.noreply.github.com>
2025-04-25 17:20:46 +0100

    config tidy

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,7 @@
+/*
+https://vitejs.dev/guide/env-and-mode.html#modes
+*/
+
 import path from "node:path";
 
 import react from "@vitejs/plugin-react";
@@ -5,45 +9,40 @@ import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-import type { PluginOption, ServerOptions, UserConfig } from "vite";
+import type { PluginOption } from "vite";
 
-// resolve here is not for alias paths
-// see paths in tsconfig.json loaded via vite-tsconfig-paths
-const resolve: UserConfig["resolve"] = {
-  conditions: ["mui-modern", "module", "browser", "development|production"],
-};
-
-export const devServer: ServerOptions = {
-  watch: {
-    ignored: [
-      "**/.nyc_output",
-      path.resolve("./coverage"),
-      path.resolve("./dist"),
-      path.resolve("./build"),
-      path.resolve("./storybook-static"),
-    ],
-  },
-};
-
-/*
-https://vitejs.dev/guide/env-and-mode.html#modes
-*/
 export default defineConfig(({ command, mode }) => {
   const isDev = command === "serve" && mode === "development";
   const isTest = command === "serve" && process.env.VITEST;
 
   const plugins: PluginOption[] = [tsconfigPaths(), react()];
-
   if (!isTest) {
     plugins.push(checker({ typescript: true }));
   }
 
   return {
-    resolve,
+    resolve: {
+      // default + mui-modern
+      conditions: ["mui-modern", "module", "browser", "development|production"],
+    },
     plugins,
     css: {
       devSourcemap: isDev,
     },
-    server: devServer,
+    server: {
+      watch: {
+        /* 
+        cannot set list of paths to watch
+        or set root/cwd to src as we need to watch other too
+        so have to add ignores, so no HMR when they change 
+        */
+        ignored: [
+          path.resolve("./coverage"),
+          path.resolve("./dist"),
+          path.resolve("./build"),
+          path.resolve("./storybook-static"),
+        ],
+      },
+    },
   };
 });
