f45170a6d97acd6ac4a8886fc2c344c4ccb7405c
anewstead <4138440+anewstead@users.noreply.github.com>
2025-04-27 00:43:31 +0100

    remove dom dump in failed tests

--- .storybook/vitest.setup.ts
+++ .storybook/vitest.setup.ts
@@ -8,10 +8,22 @@
 // More info at: https://storybook.js.org/docs/api/portable-stories/portable-stories-vitest#setprojectannotations
 
 import { setProjectAnnotations } from "@storybook/react";
+import { configure } from "@storybook/test";
 import { beforeAll } from "vitest";
 
 import * as projectAnnotations from "./preview";
 // import * as a11yAddonAnnotations from "@storybook/addon-a11y/preview";
+
+// Configure Storybook testing-library to remove DOM dump verbosity
+configure({
+  testIdAttribute: "data-testid",
+  asyncUtilTimeout: 1000,
+  getElementError: (message) => {
+    const error = new Error(message ?? "Element not found");
+    error.name = "TestingLibraryElementError";
+    return error;
+  },
+});
 
 const project = setProjectAnnotations([
   // a11yAddonAnnotations,
