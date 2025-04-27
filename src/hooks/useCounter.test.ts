4587a4d1f7cdb84882926452c236f04b9dc58191
anewstead <4138440+anewstead@users.noreply.github.com>
2025-04-27 22:02:54 +0100

    remove act

--- src/hooks/useCounter.test.ts
+++ src/hooks/useCounter.test.ts
@@ -1,23 +1,7 @@
-import { act } from "react";
-
 import { waitFor } from "@storybook/test";
 import { renderHook } from "vitest-browser-react";
 
 import { useCounter } from "./useCounter";
-
-/**
- * ## Act() currently cause a warning in the console, \
- *
- * The test do run and do pass (its all JS afterall)\
- * But the warning is misleading and annoying. \
- *
- * ## stderr | ...test reference \
- *
- * The current testing environment is not configured to support act(...) \
- *
- * Not sure on fix, waiting on:\
- * https://github.com/vitest-dev/vitest-browser-react/issues/14
- */
 
 describe("useCounter", () => {
   it("should initialize with the default value", () => {
