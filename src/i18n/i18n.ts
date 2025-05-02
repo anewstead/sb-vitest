f3342eee65761f566ba1f16c159cfaf05c4ea2e0
anewstead <4138440+anewstead@users.noreply.github.com>
2025-05-02 21:27:13 +0100

    language fix

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -1,10 +1,12 @@
 import i18n from "i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 import HttpBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
 /* ------------------------------------------------------------ */
 /**
- * Uncomment to make i18n available in the window object\
+ * For switching when running dev server\
+ * (not for storybook that has a dropdown)\
  * E.g. in browser console\
  *
  * ```js
@@ -12,25 +14,33 @@ import { initReactI18next } from "react-i18next";
  * ```
  */
 
-// declare global {
-//   interface Window {
-//     i18n: typeof i18n;
-//   }
-// }
-// window.i18n = i18n;
+declare global {
+  interface Window {
+    i18n: typeof i18n;
+  }
+}
+window.i18n = i18n;
 /* ------------------------------------------------------------ */
 
+// IMPORTANT - language configure to only be lang-COUNTRY
 void i18n
+  .use(LanguageDetector)
   .use(HttpBackend)
   .use(initReactI18next)
   .init({
     backend: {
       loadPath: "/i18n/{{lng}}/{{ns}}.json",
     },
+    detection: {
+      order: ["htmlTag", "navigator", "localStorage", "cookie"], // optional, override as needed
+      caches: [], // disable saving detected language to localStorage or cookie for clean testing
+    },
+    // load: "languageOnly", // keep if commented just in case (not obvious find in docs)
     ns: ["common", "home"],
     defaultNS: "common",
-    lng: "en-GB",
     fallbackLng: "en-GB",
+    supportedLngs: ["en-GB", "es-ES"],
+    nonExplicitSupportedLngs: false,
     interpolation: {
       escapeValue: false,
     },
